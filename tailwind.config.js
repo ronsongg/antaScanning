48f2995631b9ac86754053810f39ef773270c1b0
ronsongg <378092778@qq.com>
2026-02-07 21:16:24 +0800

    Update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./contexts/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: "class",
